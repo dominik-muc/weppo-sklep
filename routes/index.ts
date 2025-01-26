5180ba0da2671e5de01f62ccf6e62c93109f1157
Dominik Muc <git@dominikmuc.dev>
2025-01-26 12:35:50 +0100

    Added routes and views.

---
+++ routes/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const router = express.Router();
+
+router.use("/favicon.ico", express.static("public/icons/favicon.ico"));
+router.use("/site.webmanifest", express.static("public/site.webmanifest"));
+router.get("/faq", (_, res) => {
+    res.render("faq");
+});
+router.get("/about", (_, res) => {
+    res.render("about");
+});
+router.get("/", (req, res) => {
+    if (req.hostname == "Jurek Owsiak") {
+        res.send("Oddaj pieniądze żydzie");
+    } else
+        res.render("index", {
+            title: "Strona główna",
+            products: ["test", "test2", "test3"],
+        });
+});
+
+export default router;
